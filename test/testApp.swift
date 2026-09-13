a6f8d7d4b02abe7400034a15c0f81026b0a25ddb
IoannisDev <aangphurba101@gmail.com>
2026-09-13 18:42:49 -0500

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Aang phurba Sherpa on 9/13/26.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
